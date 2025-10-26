972b940796f8b1b5a208162ad4e4cb203a99b25c
Dmitriy L <ew1aad@yandex.ru>
2025-10-27 01:01:21 +0300

    1-003sql

--- lib/reg.php
+++ lib/reg.php
@@ -5,7 +5,7 @@ $login = trim(filter_var($_POST['login'], FILTER_SANITIZE_SPECIAL_CHARS));
 $clientname = trim(filter_var($_POST['clientname'], FILTER_SANITIZE_SPECIAL_CHARS));
 $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_SPECIAL_CHARS));
 $clientpassword = trim(filter_var($_POST['clientpassword'], FILTER_SANITIZE_SPECIAL_CHARS));
-echo $email;
+//echo $email;
 //echo $clientname;
 
 
@@ -18,7 +18,7 @@ if (strlen($clientname) < 4) {
     echo "Name error";
     exit;
 }
-if (strlen($email) < 4 ) {
+if (strlen($email) < 4) {
     echo "Mail error";
     exit;
 }
@@ -27,10 +27,9 @@ if (strlen($clientpassword) < 4) {
     exit;
 }
 
-
-
-
-
+//password cash
+$salt = '190530222';
+$clientpassword = md5($salt.$clientpassword);
 
 $host = 'localhost';
 $dbname = 'ew1aadgy_5';
@@ -39,19 +38,21 @@ $password = 'Dd250283';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    
+
     // Настройки PDO
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    
-    echo "Подключение успешно!";
-} catch(PDOException $e) {
+
+    //echo "Подключение успешно!";
+} catch (PDOException $e) {
     die("Ошибка подключения: " . $e->getMessage());
 }
 
 $stmt = $pdo->prepare("INSERT INTO users (login, username, email, password) VALUES (?,?,?,?)");
-echo "test успешно!";
+//echo "test успешно!";
 
-$stmt->execute([$login, $clientname, $email, $password]);
-  echo "zagruzil успешно!";  
+$stmt->execute([$login, $clientname, $email, $clientpassword]);
+//echo "zagruzil успешно!";
+
+header('location: /');
 ?>
